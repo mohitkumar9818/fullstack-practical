7e84abfe5b4b84f2006b01345606a98c056608e9
mohit kumar <143921336+mohitkumar9818@users.noreply.github.com>
2026-05-05 12:02:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-const express =require('express');
+const express = require('express');
 const app = express;
+app.get('/', async (req, res) => {  
+        const movies = await Movie.find().sort({ createdAt: -1 });
+        res.render('movies/index', { movies });
+        res.status(500).render('error',);   
+});
+app.get('/new', (req, res) => {
+    res.render('movies/new', { movie: new Movie() });
+});
 
-app.get('/')
+app.post('/', async (req, res) => {
+    const movie = new Movie(req.body); 
+        await movie.save();
+        res.redirect(`/movies/${movie._id}`);
+        res.status(400).render('movies/new');
 
-app.listen(3001);
+});
+app.get('/:id', async (req, res) => { 
+        const movie = await Movie.findById(req.params.id);    
+        res.render('movies/show', { movie });        
+    
+});
+app.get('/:id/edit', async (req, res) => {
+        const movie = await Movie.findById(req.params.id);
+        res.render('movies/edit', { movie });
+        res.status(500).render('error',);
+});
+app.put('/:id', async (req, res) => {
+        const movie = await Movie.findById(req.params.id);
+        delete req.body.title;
+    
+        await movie.save();
+        res.redirect(`/movies/${movie._id}`);
+});
+app.delete('/:id', async (req, res) => {
+        const movie = await Movie.findByIdAndDelete(req.params.id);     
+        res.redirect('/movies');
+    
+});
+app.listen(3000);
